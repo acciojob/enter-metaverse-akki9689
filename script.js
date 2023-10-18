74c9e174f47999ca84d6c8e82ae2fe87c4711a10
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-18 21:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 let para=document.getElementById("status");
 let btn=document.getElementById("enterBtn");
 btn.addEventListener('click',()=>{
-    para.innerHTML='Entered Metaverse'
+    para.innerHTML=`<h1>Entered Metaverse</h1>`
 })
